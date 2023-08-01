9bc91758c9aeb49ae151add33befd2e61b264bc2
Camilo Gonçalves <camilolgon@gmail.com>
2023-07-31 22:18:07 -0300

    Isolating the house dict conversion

---
+++ api/utils.py
@@ -0,0 +1,22 @@
+from dataclasses import asdict
+from datetime import date, datetime
+from enum import Enum
+from typing import Any, List
+
+from .models import House
+
+
+def house_factory(data: dict) -> dict:
+    def convert_value(obj: Any) -> Any:
+        if isinstance(obj, Enum):
+            return obj.value
+        elif isinstance(obj, date):
+            return datetime.combine(obj, datetime.min.time())
+        else:
+            return obj
+
+    return {k: convert_value(v) for k, v in data}
+
+
+def houses_as_dicts(houses: List[House]) -> List[dict]:
+    return [asdict(house, dict_factory=house_factory) for house in houses]
