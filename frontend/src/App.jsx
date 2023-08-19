f7caa5c294d16385fee356b386fed565a584dcac
Camilo Gonçalves <camilolgon@gmail.com>
2023-08-19 15:45:11 -0300

    Refactoring some code.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,58 +7,38 @@ import { PredictButton, PredictionArea } from "./components";
 import { v4 } from "uuid";
 
 const App = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, getValues } = useForm();
 
   const [featuresFields, setFeaturesFields] = useState(0);
   const [predictionValue, setPredictionValue] = useState(0);
 
+  const apiUrl = import.meta.env.VITE_API_URL;
+
   const getApiFieldsData = async () => {
-    const { data } = await fetch("http://127.0.0.1:8000/get-fields/").then(
-      (response) => response.json()
-    );
-
-    let groupedData = [];
-    for (let i = 0; i < Math.ceil(data.length / 3); i++) {
-      groupedData.push(data.slice(i * 3, i * 3 + 3));
-    }
-
-    setFeaturesFields(groupedData);
-  };
-
-  const transformData = ({
-    property_type,
-    neighborhood,
-    usable_area,
-    ad_date,
-    condominium_fee,
-    annual_iptu_tax,
-    bathrooms,
-    suites,
-    parking_spots,
-  }) => {
-    return {
-      property_type: property_type,
-      neighborhood: neighborhood,
-      usable_area: parseFloat(usable_area) || 0,
-      ad_date: ad_date,
-      condominium_fee: parseFloat(condominium_fee) || 0,
-      annual_iptu_tax: parseFloat(annual_iptu_tax) || 0,
-      bathrooms: parseInt(bathrooms) || 0,
-      suites: parseInt(suites) || 0,
-      parking_spots: parseInt(parking_spots) || 0,
-    };
+    await fetch(apiUrl + "get-fields/")
+      .then((response) => response.json())
+      .then(({ data }) => setFeaturesFields(data));
   };
 
   const getApiPrediction = async (data) => {
-    const response = await fetch("http://127.0.0.1:8000/predict/", {
+    const requestOptions = {
       method: "POST",
       body: JSON.stringify([data]),
       headers: {
         "Content-Type": "application/json",
       },
-    }).then((response) => response.json());
-    const salePrice = response.data[0].sale_price;
-    setPredictionValue(salePrice)
+    };
+    await fetch(apiUrl + "predict/", requestOptions)
+      .then((response) => response.json())
+      .then(({ data }) => setPredictionValue(data[0].sale_price));
+  };
+
+  const getGroupedData = () => {
+    let groupedData = [];
+    for (let i = 0; i < Math.ceil(featuresFields.length / 3); i++) {
+      groupedData.push(featuresFields.slice(i * 3, i * 3 + 3));
+    }
+    return groupedData;
   };
 
   useEffect(() => {
@@ -74,8 +54,13 @@ const App = () => {
       <h1>House Price Prediction App</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         {featuresFields &&
-          featuresFields.map((fields) => (
-            <Features fields={fields} key={v4()} register={register} />
+          getGroupedData().map((fields) => (
+            <Features
+              fields={fields}
+              key={v4()}
+              register={register}
+              getValues={getValues}
+            />
           ))}
         <PredictButton />
       </form>
