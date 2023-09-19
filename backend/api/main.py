f7b80e1b6a578ff23aa2edc3806eee2b64e21eb3
TheCamilovisk <camilolgon@gmail.com>
2023-09-19 14:46:55 -0300

    Removing unnecessary endpoint and models.

--- backend/api/main.py
+++ backend/api/main.py
@@ -7,20 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from .config import Settings
 from .load_utils import load_model, load_preprocessing_pipelines
 from .ml import make_predictions
-from .models import (
-    ApiOutput,
-    AreaFieldProps,
-    CurrencyFieldProps,
-    DateFieldProps,
-    FeatureField,
-    FeatureFieldTypes,
-    House,
-    PropertyType,
-    RangeFieldProps,
-    SalePrice,
-    SelectFieldProps,
-    TextFieldProps,
-)
+from .models import House, SalePicesPredictions, OptionsListOutput, PropertyType
 from .utils import houses_as_dicts
 
 predictor = None
@@ -65,124 +52,17 @@ api_router = APIRouter()
 
 
 @api_router.post("/predict/")
-async def predict(houses: List[House]) -> ApiOutput:
+async def predict(houses: List[House]) -> SalePicesPredictions:
     houses_dicts = houses_as_dicts(houses)
     sales_prices = make_predictions(
         houses_dicts, predictor, features_transform, target_transform
     )
-    return ApiOutput(
-        data=[SalePrice(sale_price) for sale_price in sales_prices], status="success"
-    )
+    return SalePicesPredictions(sale_prices=sales_prices)
 
 
-@api_router.get("/get-fields/")
-async def get_fields() -> ApiOutput:
-    property_type = FeatureField(
-        type=FeatureFieldTypes.select,
-        props=SelectFieldProps(
-            label="Property Type",
-            id="property_type",
-            name="property_type",
-            options=[obj for obj in PropertyType],
-        ),
-    )
-
-    neighborhood = FeatureField(
-        type=FeatureFieldTypes.text,
-        props=TextFieldProps(
-            label="Neighborhood",
-            id="neighborhood",
-            name="neighborhood",
-            placeholder="Neighborhood",
-        ),
-    )
-
-    usable_area = FeatureField(
-        type=FeatureFieldTypes.area,
-        props=AreaFieldProps(
-            label="Usable Area", id="usable_area", name="usable_area", placeholder=0
-        ),
-    )
-
-    ad_date = FeatureField(
-        type=FeatureFieldTypes.date,
-        props=DateFieldProps(
-            label="Ad Date",
-            id="ad_date",
-            name="ad_date",
-        ),
-    )
-
-    condominium_fee = FeatureField(
-        type=FeatureFieldTypes.currency,
-        props=CurrencyFieldProps(
-            label="Condominium Fee",
-            id="condominium_fee",
-            name="condominium_fee",
-            placeholder=0.0,
-        ),
-    )
-
-    annual_iptu_tax = FeatureField(
-        type=FeatureFieldTypes.currency,
-        props=CurrencyFieldProps(
-            label="Annual IPTU tax",
-            id="annual_iptu_tax",
-            name="annual_iptu_tax",
-            placeholder=0.0,
-        ),
-    )
-
-    bathrooms = FeatureField(
-        type=FeatureFieldTypes.range,
-        props=RangeFieldProps(
-            label="Bathrooms",
-            id="bathrooms",
-            name="bathrooms",
-            min=0,
-            max=7,
-            defaultValue=1,
-        ),
-    )
-
-    suites = FeatureField(
-        type=FeatureFieldTypes.range,
-        props=RangeFieldProps(
-            label="Suites",
-            id="suites",
-            name="suites",
-            min=0,
-            max=6,
-            defaultValue=0,
-        ),
-    )
-
-    parking_spots = FeatureField(
-        type=FeatureFieldTypes.range,
-        props=RangeFieldProps(
-            label="Parking Spots",
-            id="parking_spots",
-            name="parking_spots",
-            min=0,
-            max=7,
-            defaultValue=1,
-        ),
-    )
-
-    return ApiOutput(
-        data=[
-            property_type,
-            neighborhood,
-            usable_area,
-            ad_date,
-            condominium_fee,
-            annual_iptu_tax,
-            bathrooms,
-            suites,
-            parking_spots,
-        ],
-        status="success",
-    )
+@api_router.get("/property-types/")
+async def property_types() -> OptionsListOutput:
+    return OptionsListOutput(options=[opt for opt in PropertyType])
 
 
 app.include_router(api_router, prefix="/api")
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,4 @@
-import datetime
-import typing
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from datetime import date
 from enum import Enum
 from typing import List, Optional
@@ -29,7 +27,7 @@ class FeatureFieldTypes(str, Enum):
 @dataclass
 class House:
     neighborhood: str
-    property_type: PropertyType
+    property_type: PropertyType = PropertyType.apartment
     usable_area: float
     ad_date: date
     bathrooms: Optional[float] = None
@@ -38,73 +36,13 @@ class House:
     parking_spots: Optional[float] = None
     condominium_fee: Optional[float] = None
     annual_iptu_tax: Optional[float] = None
+    
+
+@dataclass
+class SalePicesPredictions:
+    sale_prices: List[float]
 
 
 @dataclass
-class SalePrice:
-    sale_price: float
-
-
-@dataclass
-class PropertyTypesList:
-    property_types: List[PropertyType] = field(
-        default_factory=lambda: [obj for obj in PropertyType]
-    )
-
-
-@dataclass
-class FeatureFieldPropsBase:
-    label: str
-    id: str
-    name: str
-
-
-@dataclass
-class SelectFieldProps(FeatureFieldPropsBase):
+class OptionsListOutput:
     options: List[str]
-
-
-@dataclass
-class TextFieldProps(FeatureFieldPropsBase):
-    placeholder: str
-
-
-@dataclass
-class AreaFieldProps(FeatureFieldPropsBase):
-    placeholder: int
-
-
-@dataclass
-class DateFieldProps(FeatureFieldPropsBase):
-    defaultDate: datetime.date = datetime.date.today()
-
-
-@dataclass
-class CurrencyFieldProps(FeatureFieldPropsBase):
-    placeholder: float
-
-
-@dataclass
-class RangeFieldProps(FeatureFieldPropsBase):
-    min: int
-    max: int
-    defaultValue: int
-
-
-@dataclass
-class FeatureField:
-    type: FeatureFieldTypes
-    props: typing.Union[
-        SelectFieldProps,
-        TextFieldProps,
-        AreaFieldProps,
-        DateFieldProps,
-        CurrencyFieldProps,
-        RangeFieldProps,
-    ]
-
-
-@dataclass
-class ApiOutput:
-    data: typing.Union[List[SalePrice], List[FeatureField]]
-    status: str
